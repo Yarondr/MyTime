1aed2b3dbda813041da25a49e6aa405a1c9761bf
Yarondr <yarondrukh@gmail.com>
2023-02-04 03:50:57 +0200

    Change activity main to activity overview Started to work on the overview activity ui

--- app/src/main/java/me/yarond/mytime/OverviewActivity.kt
+++ app/src/main/java/me/yarond/mytime/OverviewActivity.kt
@@ -3,9 +3,9 @@ package me.yarond.mytime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OverviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_overview)
     }
 }
