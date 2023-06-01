7fc67a82929c0cdfffe7b58a712bb2925081a0fa
Yarondr <yarondrukh@gmail.com>
2023-06-01 22:36:07 +0300

    Create diagram file Remove default fragment activity

--- app/src/main/java/me/yarond/mytime/ui/fragmentTypes/SidebarFragmentActivity.kt
+++ app/src/main/java/me/yarond/mytime/ui/fragmentTypes/SidebarFragmentActivity.kt
@@ -2,10 +2,11 @@ package me.yarond.mytime.ui.fragmentTypes
 
 import android.widget.ImageButton
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentActivity
 import com.google.android.material.navigation.NavigationView
 import me.yarond.mytime.ui.Sidebar
 
-open class SidebarFragmentActivity(): DefaultFragmentActivity(), Sidebar {
+open class SidebarFragmentActivity(): FragmentActivity(), Sidebar {
     override lateinit var drawerLayout: DrawerLayout
     override lateinit var navigationView: NavigationView
     override lateinit var sidebarButton: ImageButton
